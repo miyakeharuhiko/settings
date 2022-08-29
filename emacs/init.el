f3ffbb98ae5f8b6382571819dd526f1f460c7721
三宅春彦 <miyake@MacBookAir.local>
2022-08-29 09:45:18 +0900

    modified:   .gitignore 	renamed:    indent-guide-master/.gitignore -> emacs/indent-guide-master/.gitignore 	renamed:    indent-guide-master/Readme.org -> emacs/indent-guide-master/Readme.org 	renamed:    indent-guide-master/indent-guide.el -> emacs/indent-guide-master/indent-guide.el 	renamed:    indent-guide-master/screencast.gif -> emacs/indent-guide-master/screencast.gif 	renamed:    indent-guide-master/screencast2.gif -> emacs/indent-guide-master/screencast2.gif 	renamed:    init.el -> emacs/init.el 	renamed:    lisp/cmake-mode.el -> emacs/lisp/cmake-mode.el 	renamed:    lisp/swap-buffer.el -> emacs/lisp/swap-buffer.el 	new file:   setup.sh 	new file:   tmux/.tmux.conf.org 	new file:   tmux/tmux-pane-border.sh 	new file:   tmux/tmux.conf 	Modified directory tree structure.

--- emacs/init.el
+++ emacs/init.el
@@ -1,8 +1,8 @@
-;;(package-initialize)
-;;(setq package-archives
-;;      '(("gnu" . "http://elpa.gnu.org/packages/")
-;;	("melpa" . "http://melpa.org/packages/")
-;;	("org" . "http://orgmode.org/elpa/")))
+(package-initialize)
+(setq package-archives
+      '(("gnu" . "http://elpa.gnu.org/packages/")
+	("melpa" . "http://melpa.org/packages/")
+	("org" . "http://orgmode.org/elpa/")))
 
 
 (add-to-list 'load-path "~/.emacs.d/indent-guide-master/")
---
+++ setup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+ln -si ./tmux/tmux.conf  ~/.tmux.conf
+ln -si ./emacs  ~/.emacs.d
